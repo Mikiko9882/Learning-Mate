a92e6556812f724fd4e16f97bbb925289b52fa31
Mikiko9882 <teddy.0802.t@gmail.com>
2024-04-06 18:08:10 +0900

    バリデーションを追加

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -6,6 +6,8 @@ class TestResult < ApplicationRecord
 
   before_save :calculate_achievement_rate
 
+  validate :achievement_rate_within_range
+
   def display_preparation_time
     hours = preparation_time_minutes / 60
     minutes = preparation_time_minutes % 60
@@ -19,6 +21,15 @@ class TestResult < ApplicationRecord
 
     max_score_value = max_score.max_score
     self.achievement_rate = ((score.to_f / max_score_value) * 100).round
+  end
+
+  def achievement_rate_within_range
+    if score.present? && max_score.present?
+      calculated_achievement_rate = ((score.to_f / max_score.max_score) * 100).round
+      if calculated_achievement_rate > 100
+        errors.add(:score, "達成率は100%を超えることはできません")
+      end
+    end
   end
 
   def self.user_test_results(user)
