68dc61c15d0d0eb0bca5be7b48922492c2db5b78
Mikiko9882 <teddy.0802.t@gmail.com>
2024-04-06 21:13:51 +0900

    編集まで

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -20,11 +20,14 @@ class TestResultsController < ApplicationController
   def show 
   end
 
-  def edit; end
+  def edit
+    @test_result = current_user.test_results.find(params[:id])
+  end
 
   def update
+    @test_result = current_user.test_results.find(params[:id])
     if @test_result.update(test_result_params)
-      redirect_to test_results_path, success: t('defaults.message.updated', item: TestResult.model_name.human)
+      redirect_to @test_result, success: t('defaults.message.updated', item: TestResult.model_name.human)
     else
       flash.now['danger'] = t('defaults.message.not_updated', item: TestResult.model_name.human)
       render :edit
@@ -32,10 +35,11 @@ class TestResultsController < ApplicationController
   end
 
   def destroy
+    @test_result = current_user.test_results.find(params[:id])
     @test_result.destroy!
     redirect_to test_results_path, success: t('defaults.message.deleted', item: TestResult.model_name.human)
   end
-  
+
   private
 
   def test_result_params
